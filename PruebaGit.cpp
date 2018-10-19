95b1c443c605886f0c36c8c92a3532d16104e084
Erick Carrasco <erick.e.carrasco88@gmail.com>
2018-10-18 18:05:23 -0600

    Se ha realizado una prueba de github!

--- PruebaGit.cpp
+++ PruebaGit.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(){
+
+	cout<< "Esta es una prueba para github!";
+	cout<< endl;
+	cout<< "Gracias por usar github :) " << endl;
+	return 0;
+}
